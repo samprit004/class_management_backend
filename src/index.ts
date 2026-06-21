8616f99dbb557bb3d84a08d9b3eafb3dcaadf087
samprit004 <sampritdas2004@gmail.com>
2026-06-22 00:47:08 +0530

    implemented GET all sub route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import cors from "cors";
+import Subjectsrouter from "./routes/subjects";
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}
+
+));
+
+app.use('/api/subjects', Subjectsrouter)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
